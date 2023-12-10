a3d7eaf196e268c77bdfa5d70118c50c7140efb2
DESKTOP-FRZNGAQ\Admin <lilybean4868@outlook.com>
2023-12-09 23:37:22 -0500

    no message

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -106,12 +106,19 @@
                         <a class="dropdown-item" href="{{ url('world') }}">
                             Encyclopedia
                         </a>
+
                         <a class="dropdown-item" href="{{ url('prompts/prompts') }}">
                             Prompts
                         </a>
+
                         <a class="dropdown-item" href="{{ url('shops') }}">
                             Shops
                         </a>
+
+			<a class="dropdown-item" href="{{ url('corruption') }}">
+                            Corruption Guide
+                        </a>
+
                     </div>
                 </li>
                 <li class="nav-item">
